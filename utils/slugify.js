4cf59aa515799e10edde93e9cde0dcf8c966a054
Soufiane Maakouk <soufianemaakouk@gmail.com>
2025-08-14 15:24:09 +0200

    Create slugify.js

---
+++ utils/slugify.js
@@ -0,0 +1,8 @@
+export function slugify(str = "") {
+  return String(str)
+    .toLowerCase()
+    .trim()
+    .normalize("NFKD").replace(/[\u0300-\u036f]/g, "")
+    .replace(/[^a-z0-9]+/g, "-")
+    .replace(/(^-|-$)/g, "");
+}
